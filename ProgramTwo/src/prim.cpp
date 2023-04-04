944bf3b954a0ed55f75d391138ff4e5d9015c1e1
troync <32421060+troync@users.noreply.github.com>
2023-04-04 05:26:58 -0400

    Rough Prim Imp
    I still need to test; I'll do it once you put in the test files and fix any issues.

--- ProgramTwo/src/prim.cpp
+++ ProgramTwo/src/prim.cpp
@@ -12,6 +12,7 @@
 #include "../include/Graph.h"
 #include "../include/Edge.h"
 #include "../include/Vertex.h"
+#include "../include/BinaryHeap.h"
 
 /**
  * @brief Computes the Minimum Spanning Tree using Prim's algorithm
@@ -24,8 +25,43 @@ std::vector<Edge*> computeMST(Graph* graph) {
     std::vector<Edge*> mst;
     mst.reserve(graph->vertices.size() - 1);
 
-    // implementation here
-    
+    // We pick a start vertex
+    Vertex* startV = graph->vertices.at(0);
+    // Look at the edges incident to the start vertex and put them in the min heap priority queue: Q
+    BinaryHeap Q = BinaryHeap(startV->incidentEdges);
+    // Mark the start vertex as in the mst
+    graph->vertices.at(0)->marked = true;
+    // While there are still edges in Q...
+    while(Q.heapList.size() > 0){
+        // Remove the minimum edge
+        Edge* minimumEdge = Q.removeMin();
+        // Ensure that the edge points to a vertex not in the mst
+        Vertex* minimumEdgeDest = nullptr;
+        if(!graph->vertices.at(minimumEdge->source)->marked){
+            minimumEdgeDest = graph->vertices.at(minimumEdge->source);
+        } else if (!graph->vertices.at(minimumEdge->destination)->marked){
+            minimumEdgeDest = graph->vertices.at(minimumEdge->destination);
+        }
+        // If the edge points to a vertex no in the mst...
+        if(minimumEdgeDest != nullptr){
+            // Add the edge to the mst
+            mst.push_back(minimumEdge);
+            // Mark the vertex as in the mst
+            graph->vertices.at(minimumEdgeDest->id)->marked = true;
+            // Find the incident edges to this new vertex
+            std::vector<Edge*> newEdges = minimumEdgeDest->incidentEdges;
+            // For all of these incident edges...
+            for(int i = 0; i <= newEdges.size(); i++){
+                // Find the end point of the edge that is not the recently marked vertex
+                Vertex* newEdgeDest = graph->vertices.at(newEdges.at(i)->source);
+                if(newEdgeDest->id == minimumEdgeDest->id)
+                    newEdgeDest = graph->vertices.at(newEdges.at(i)->destination);
+                // If this vertex is not marked, add the edge to Q
+                if (!newEdgeDest->marked)
+                    Q.addEdge(newEdges.at(i));
+            }
+        }
+    }
 
     return mst;
 }
