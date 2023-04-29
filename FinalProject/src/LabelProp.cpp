d8051e666f79fa5fa4a5fffdb505e863193480d9
troync <32421060+troync@users.noreply.github.com>
2023-04-28 21:38:55 -0400

    Made Label Prop Work

--- FinalProject/src/LabelProp.cpp
+++ FinalProject/src/LabelProp.cpp
@@ -21,37 +21,22 @@
 using namespace std;
 
 //This just grabs the node opposite of vertex v through Edge e in Graph g
-Vertex* getOpposite(Vertex& v, Edge& e, Graph& graph){
-    Vertex* r = graph.vertices[e.destination];
+int getOpposite(Vertex* v, Edge* e, Graph graph){
+    Vertex* r = graph.vertices[e->destination];
     // std::cout << "Vertex ID: " << r->id << std::endl;
-    if(r->id == v.id)
-        return graph.vertices[e.source];
-    return r;
+    if(r->id == v->id)
+        return graph.vertices[e->source]->id;
+    return r->id;
 }
 
-std::vector<int> fillUnique(int length, int min, int max)
+std::vector<int> generateLabels(int length)
 {
     std::vector<int> ret = std::vector<int>();
     ret.resize(length);
 
-    // Store each random number generated
-    int new_random;
-
-    // Helps to verify whether the random number generated is unique
-    bool unique;
-    
     for (int i = 0; i < length; i++)
     {
-      do
-      {
-        new_random = (rand() % (max - min + 1)) + min;
-
-        unique = true;
-        for (int j = 0; j < i; j++)
-          if (ret.at(j) == new_random) unique = false;
-      }
-      while (!unique);
-      ret.at(i) = new_random;
+      ret.at(i) = i;
     }
 
     return ret;
@@ -69,74 +54,57 @@ vector<Vertex*> getConnectedComponents(Graph& graph, const unsigned int threads)
     // This keeps track of the start vertex for each connected component
     vector<Vertex*> connectedComponentsList;
 
+    // The list of labels for all of the vertexes
+    std::vector<int> labels = generateLabels(graph.vertices.size());
+    
+
     //We keep track of if there was a change
     bool change;
-
-    // Counter to curb the runtime
-    int maxIterations = 100;
 
     // We iterate through every vertex and set its label to match the majority of its neiighbors
     // until there are no updates
     do {
         change = false;
-        std::vector<int> randomizedVertices = fillUnique(graph.vertices.size(), 0, graph.vertices.at(graph.vertices.size() - 1)->id);
-        for (int i = 0; i < randomizedVertices.size(); i++) {
-            Vertex* v = graph.vertices[randomizedVertices.at(i)];
-            // std::cout << "ID of the current Vertex: " << v->id << std::endl;
-            // std::cout << "Label of the vertex: " << v->label << std::endl;
-            std::map<int, int> counter;
-            // vector<Edge*> incidentEdges = v->incidentEdges;
+        // Process current frontier
+        #pragma omp parallel num_threads(threads)
+        #pragma omp for
+        for (int i = 0; i < graph.vertices.size(); i++) {
+            Vertex* v = graph.vertices[i];
 
-            //Here we are seeing what all the neighbors are labled as
+            unsigned int min = labels[v->id];
+            //Here we find the minimum label between a given vertex and it's neighbors 
             for (int edge = 0; edge < v->incidentEdges.size(); edge++) {
-                int edgePointer = v->incidentEdges[edge];
-                // cout << "Current edge: " << randomizedVertices.at(i) << " to " << edgePointer << endl;
-                // int oppositeVertex = graph.edges[edge].;
-                Vertex* x = graph.vertices.at(edgePointer);
-                counter[x->label]++;
+                int neighborID = v->incidentEdges[edge];
+                if (labels[neighborID] < min)
+                    min = labels[neighborID];
             }
 
-            //Here we see what the which lable appears the most
-            std::map<int,int>::iterator max = counter.begin();
-            for (std::map<int,int>::iterator it = ++(counter.begin()); it != counter.end(); ++it) {
-                // std::cout << "Current max: " << max->first << " " << max->second << " Check against: " << it->first << " " << it->second << std::endl;
-                if (it->second > max->second)
-                    max = it;
-                // If we have the same amount of two labels, the smaller label value is chosen
-                if (it->second == max->second)
-                {
-                    if (it->first < max->first)
-                    {
-                        max = it;
-                    }
+            if (labels[v->id] != min) {
+                    labels[v->id] = min;
+                    change = true;
+            }
+
+            //Set all the neighbors to equal the new min value
+            for (int edge = 0; edge < v->incidentEdges.size(); edge++) {
+                int neighborID = v->incidentEdges[edge];
+                if (labels[neighborID] > min){
+                    labels[neighborID] = min;
+                    change = true;
                 }
             }
 
-            // We update the label if it does not match the majority of its neighbors
-            if (v->label != max->first) {
-                change = true;
-                v->label = max->first;
-            }
-        }
-        maxIterations--;
-        if (maxIterations == 0)
-        {
-            // std::cout << "Max number of iterations reached" << std::endl;
-            change = false;
         }
     } while (change);
 
-    // std::cout << "Finished the vertex updating" << std::endl;
-
     //Now we collect the unique labels as those make up our connected components list
     connectedComponentsList.push_back(graph.vertices[0]);
-    // std::cout << graph.vertices[0]->label << std::endl;
+   
     for (int i = 1; i < graph.vertices.size(); i++) {
-        int label = graph.vertices[i]->label;
-        // std::cout << "Label of the Vertex: " << label << " Vertex ID: " << graph.vertices[i]->id << std::endl;
+
+        int label = labels[i];
         bool inList = false;
         for (int comp = 0; comp < connectedComponentsList.size(); comp++) {
-            if (label == connectedComponentsList[comp]->label)
+            if (label == labels[connectedComponentsList[comp]->id])
                 inList = true;
         }
 
